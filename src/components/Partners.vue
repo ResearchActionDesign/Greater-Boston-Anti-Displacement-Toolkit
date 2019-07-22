55c0bc180c20a33a026565389182894f389dd1e1
Tim Stallmann <tim@stallmann.mayfirst.org>
2019-07-22 11:36:58 -0400

    style updates

--- src/components/Partners.vue
+++ src/components/Partners.vue
@@ -2,15 +2,28 @@
     <article class="partners">
         <div v-grid="'1fr 1fr 1fr 1fr 1fr'" class="partnerLogos">
             <a href="https://www.challiance.org/"><img src="./../assets/partnerLogos/CHA_logo.png" alt="Cambridge Health Alliance Logo"></a>
-            <a href="https://www.colab.mit.edu/"><img src="./../assets/partnerLogos/colab-logo.png" alt="MIT Community Innovators Lab Logo"></a>
             <a href="https://www.challiance.org/community/everett-health-partnership"><img src="./../assets/partnerLogos/echp-logo-no-sac-copy2.png" alt="Everett Community Health Partnership Logo"></a>
             <a href="https://www.greenrootschelsea.org/"><img src="./../assets/partnerLogos/GreenRootsLogo_flat.jpg" alt="Green Roots Logo"></a>
             <a href="https://rad.cat/"><img src="./../assets/partnerLogos/RAD_logo3_large---transparency.png" alt="Research Action Design Logo"></a>
+            <a href="https://www.colab.mit.edu/"><img src="./../assets/partnerLogos/colab-logo.png" alt="MIT Community Innovators Lab Logo"></a>
         </div>
         <span class="partnersMessage">
             <p> <strong>{{ $t('partnersDisclaimer.SurdnaFoundation') }}</strong> {{ $t('partnersDisclaimer.Surdnacontent') }}</p>
             <p> <strong>{{ $t('partnersDisclaimer.CreativeCommonsAttribution') }}</strong> {{ $t('partnersDisclaimer.AttributionContent') }}</p>
-            <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /></a>
+            <a class="license" rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="./../assets/icons/cc.svg"/><img alt="Attribution" style="border-width:0" src="./../assets/icons/by.svg"/><img alt="Non-Commercial" style="border-width:0" src="./../assets/icons/nc.svg"/><img alt="Share Alike" style="border-width:0" src="./../assets/icons/sa.svg"/></a>
         </span>
     </article>
 </template>
+
+<style scoped>
+p {
+    width: 80%;
+    margin: 1em auto;
+    text-align: left;
+}
+.license img {
+    margin: 0.25em;
+    width: 24px;
+    height: 24px;
+}
+</style>
