43e985de7e409dc9d8c554685d4c7b586a0d1ab3
Tim Stallmann <tim@stallmann.mayfirst.org>
2019-08-24 13:02:27 -0700

    Proofreading & translation edits

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,10 @@
     <Header></Header>
     <nav class="nav" v-grid="'1fr 1fr 1fr 1fr'">
       <router-link  :to="{ name: 'Overview', params: { lang: this.$i18n.locale }}">{{ $t('overview') }}</router-link>
-      <router-link :to="{ name: 'Toolkit', params: { lang: this.$i18n.locale }}" class="toolkitsNav">{{
+      <router-link :to="{ name: 'Toolkit' + (this.$i18n.locale === 'es' ? '-es' : ''), params: { lang: this.$i18n.locale }}" class="toolkitsNav">{{
         $t('toolkit') }}</router-link>
-      <router-link :to="{ name: 'Film', params: { lang: this.$i18n.locale }}" class="toolkitsNav">{{ $t('film') }}</router-link>
-      <router-link :to="{ name: 'Resources', params: { lang: this.$i18n.locale }}">{{ $t('resources') }}</router-link>
+      <router-link :to="{ name: 'Film' + (this.$i18n.locale === 'es' ? '-es' : ''), params: { lang: this.$i18n.locale }}" class="toolkitsNav">{{ $t('film') }}</router-link>
+      <router-link :to="{ name: 'Resources' + (this.$i18n.locale === 'es' ? '-es' : ''), params: { lang: this.$i18n.locale }}">{{ $t('resources') }}</router-link>
     </nav>
     <router-view/>
     <Partners></Partners>
